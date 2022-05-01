759acc6958a6acf276dee269fb795a982dfc5ffc
Simon Atticus <77017038+AddictedCoins@users.noreply.github.com>
2022-05-01 09:29:45 -0700

    Delete src/main/java/Addicted2Coins/iplog directory

--- src/main/java/Addicted2Coins/iplog/Database.java
+++
@@ -1,6 +0,0 @@
-package jja8.iplog;
-
-public class Database {
-    public String city = "plugins/IPcheckur/GeoLite2-City.mmdb";
-    public String country = "plugins/IPcheckur/GeoLite2-Country.mmdb";
-}
